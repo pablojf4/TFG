dc4b835a0dd8cdb01887e0d55c32fa52fb2f3399
Pablo <pablojf4@gmail.com>
2025-04-17 19:38:38 +0200

    balas del personaje y balas enemigas son destruidas al salir de la escena visible por el jugador

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -12,6 +12,19 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        //Obtiene la posición actual de la bala
+        Vector2 pos = transform.position;
+
+        //Encuentra los límites de la pantalla para el movimiento de la bala
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));// Este es el punto (esquina) inferior izquierda de la pantalla.
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));//Este es el punto (esquina) superior derecha de la pantalla.
+
+        //Se revisa si la posición de la bala en el eje y es superior al margen superior de la cámara
+        //Si es superior se destruye la bala
+        if (pos.y > max.y)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -12,6 +12,19 @@ public class EnemyBullet : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        //Obtiene la posición actual de la bala
+        Vector2 pos = transform.position;
+
+        //Encuentra los límites de la pantalla para el movimiento de la bala enemiga
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));// Este es el punto (esquina) inferior izquierda de la pantalla.
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));//Este es el punto (esquina) superior derecha de la pantalla.
+
+        //Se revisa si la posición de la bala enemiga en el eje y es menor al margen inferior de la cámara
+        //Si es superior se destruye la bala enemiga
+        if (pos.y < min.y)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
